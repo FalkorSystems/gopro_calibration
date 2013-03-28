1368a9e443fd0386b08ef1c3c470666c03656dbd
Sameer Parekh <sameer@falkorsystems.com>
2013-03-27 23:06:54 -0400

    rename mac icon

--- DeFisheye/setup.py
+++ DeFisheye/setup.py
@@ -10,7 +10,7 @@ if (platform.system() == "Darwin"):
                'packages': 'PySide,yaml',
                'includes': 'cv,cv2',
                'argv_inject': '--gui',
-               'iconfile': 'pixmaps/icon.icns',
+               'iconfile': 'pixmaps/defisheye.icns',
                'plist': {
                    'CFBundleIdentifier': 'com.falkorsystems.defisheye',
                    'CFBundleName': 'DeFisheye',
